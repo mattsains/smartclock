190b61efd6eec6eb3b6c010b6130b0c0fa934e50
Matthew Sainsbury <matthew@sainsbury.io>
2019-07-14 21:12:00 -0700

    Add clock graphic

--- src/components/time.js
+++ src/components/time.js
@@ -7,10 +7,104 @@ module.exports = class Time {
         this.height = height;
     }
 
+    getX(angle) {
+        return -Math.sin(angle + Math.PI)
+    }
+
+    getY(angle) {
+        return Math.cos(angle + Math.PI)
+    }
+
+    clock(ctx) {
+        var x, y, i
+        var now = new Date()
+
+        ctx.save()
+
+        ctx.translate(160, 160)
+        ctx.beginPath()
+        ctx.lineWidth = 14
+        ctx.strokeStyle = '#325FA2'
+        ctx.fillStyle = '#eeeeee'
+        ctx.arc(0, 0, 142, 0, Math.PI * 2, true)
+        ctx.stroke()
+        ctx.fill()
+
+        // Hour marks
+        ctx.lineWidth = 8
+        ctx.strokeStyle = '#000000'
+        for (i = 0; i < 12; i++) {
+            x = this.getX(Math.PI / 6 * i)
+            y = this.getY(Math.PI / 6 * i)
+            ctx.beginPath()
+            ctx.moveTo(x * 100, y * 100)
+            ctx.lineTo(x * 125, y * 125)
+            ctx.stroke()
+        }
+
+        // Minute marks
+        ctx.lineWidth = 5
+        ctx.strokeStyle = '#000000'
+        for (i = 0; i < 60; i++) {
+            if (i % 5 !== 0) {
+                x = this.getX(Math.PI / 30 * i)
+                y = this.getY(Math.PI / 30 * i)
+                ctx.beginPath()
+                ctx.moveTo(x * 117, y * 117)
+                ctx.lineTo(x * 125, y * 125)
+                ctx.stroke()
+            }
+        }
+
+        var sec = now.getSeconds()
+        var min = now.getMinutes()
+        var hr = now.getHours() % 12
+
+        ctx.fillStyle = 'black'
+
+        // Write hours
+        x = this.getX(hr * (Math.PI / 6) + (Math.PI / 360) * min + (Math.PI / 21600) * sec)
+        y = this.getY(hr * (Math.PI / 6) + (Math.PI / 360) * min + (Math.PI / 21600) * sec)
+        ctx.lineWidth = 14
+        ctx.beginPath()
+        ctx.moveTo(x * -20, y * -20)
+        ctx.lineTo(x * 80, y * 80)
+        ctx.stroke()
+
+        // Write minutes
+        x = this.getX((Math.PI / 30) * min + (Math.PI / 1800) * sec)
+        y = this.getY((Math.PI / 30) * min + (Math.PI / 1800) * sec)
+
+        ctx.lineWidth = 10
+        ctx.beginPath()
+        ctx.moveTo(x * -28, y * -28)
+        ctx.lineTo(x * 112, y * 112)
+        ctx.stroke()
+
+        // Write seconds
+        x = this.getX(sec * Math.PI / 30)
+        y = this.getY(sec * Math.PI / 30)
+        ctx.strokeStyle = '#D40000'
+        ctx.fillStyle = '#D40000'
+        ctx.lineWidth = 6
+        ctx.beginPath()
+        ctx.moveTo(x * -30, y * -30)
+        ctx.lineTo(x * 83, y * 83)
+        ctx.stroke()
+        ctx.beginPath()
+        ctx.arc(0, 0, 10, 0, Math.PI * 2, true)
+        ctx.fill()
+        ctx.beginPath()
+        ctx.arc(x * 95, y * 95, 10, 0, Math.PI * 2, true)
+        ctx.stroke()
+        ctx.fillStyle = '#555'
+        ctx.arc(0, 0, 3, 0, Math.PI * 2, true)
+        ctx.fill()
+
+        ctx.restore();
+    }
+
     update() {
-        const date = new Date();
-        
-        this.time = "" + date.getHours() + ":" + ("0" + date.getMinutes()).substr(-2);
-        this.ctx.fillText(this.time, this.x, this.y);
+        this.clock(this.ctx);
     }
 }
--- src/index.js
+++ src/index.js
@@ -8,23 +8,22 @@ if (typeof document === 'undefined') {
     canvas.width = 480;
     canvas.height = 320;
     canvas.style.position = 'absolute';
-    canvas.style.border = '1px solid';
+    canvas.style.border = '2px solid #f00';
+    canvas.style.backgroundColor = '#000000'
     document.body.appendChild(canvas);
 }
 const ctx = canvas.getContext("2d");
 
 const Time = require('./components/time');
 
-// const updateInterval = 500;
-
-const timeComponent = new Time(ctx, 0, 70, 480, 180);
+const timeComponent = new Time(ctx, 100, 70, 480, 180);
 
 setInterval(() => {
     ctx.clearRect(0, 0, 480, 320);
     timeComponent.update();
     if (typeof document === 'undefined') {
         const fs = require("fs");
-        const fb = fs.openSync("/dev/fb0", "w"); // where /dev/fb1 is the path to your fb device
+        const fb = fs.openSync("/dev/fb0", "w");
         const buff = canvas.toBuffer("raw");
         fs.writeSync(fb, buff, 0, buff.byteLength, 0);
     }
